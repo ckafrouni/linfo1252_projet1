0811cdc28a1a5919f0e053ce368cc51b76555746
Christophe Kafrouni <ckafrouni@proton.me>
2023-11-29 13:52:56 +0000

    Update .gitignore and experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -2,54 +2,36 @@
 # ---------------------------------------------
 # SET TEST ENVIRONMENT
 # ---------------------------------------------
+PROJECT_DIR="$(pwd)"
+DIR_TARGET="$PROJECT_DIR/target"
+DIR_TESTS="$PROJECT_DIR/src/test"
 
-# make
+CSV="$PROJECT_DIR/tmp.csv"
 
-# check if target directory exists
-if [ ! -d "target" ]; then
-    echo -e "\e[31mError: target directory not found. Please compile the project first.\e[0m"
-    exit 1
-fi
+# ---------------------------------------------
+# EXPERIMENTS
+# ---------------------------------------------
+EXPERIMENTS_NAMES=(
+    "Readers/writers tests"     # 1
+    "Producers/consumers tests" # 2
+    "Philosophers tests"        # 3
 
-# check if target directory is empty
-if [ ! "$(ls -A target)" ]; then
-    echo -e "\e[31mError: target directory is empty. Please compile the project first.\e[0m"
-    exit 1
-fi
+)
 
-echo -e "\n\e[32m==========================\nSetup Environment\n==========================\e[0m"
-PROJECT_DIR=$(pwd)
-DIR_TARGET=$PROJECT_DIR'/target'
-DIR_TESTS=$PROJECT_DIR'/src/test'
-DIR_DATA=$PROJECT_DIR'/data'
-mkdir -p $DIR_DATA
+EXPERIMENTS_CMDS=(
+    "$DIR_TESTS/perf_readers-writers.sh $DIR_TARGET $CSV"
+    "$DIR_TESTS/perf_producers-consumers.sh $DIR_TARGET $CSV"
+    "$DIR_TESTS/perf_philosophers.sh $DIR_TARGET $CSV"
+)
 
+# ---------------------------------------------
+# RUN EXPERIMENTS
+# ---------------------------------------------
+# CHANGE THIS TO RUN A DIFFERENT EXPERIMENT
 EXPERIMENT=1
 
-# ---------------------------------------------
-# RUN TESTS READERS/WRITERS
-# ---------------------------------------------
-if [ $EXPERIMENT -eq 1 ]; then
-    echo -e "\n\e[32m==========================\nReaders/writers tests\n==========================\e[0m"
-    bash $DIR_TESTS/perf_readers-writers.sh $DIR_TARGET $DIR_DATA/res_readers-writers.csv
-    cat $DIR_DATA/res_readers-writers.csv
-fi
+echo -e "\e[32m==========================\n#$EXPERIMENT ${EXPERIMENTS_NAMES[$EXPERIMENT]}\n==========================\e[0m"
+bash ${EXPERIMENTS_CMDS[$EXPERIMENT]}
+echo -e "\e[32m==========================\e[0m\n"
 
-
-# ---------------------------------------------
-# RUN TESTS PRODUCERS/CONSUMERS
-# ---------------------------------------------
-if [ $EXPERIMENT -eq 2 ]; then
-    echo -e "\n\e[32m==========================\nProducers/consumers tests\n==========================\e[0m"
-    bash $DIR_TESTS/perf_producers-consumers.sh $DIR_TARGET $DIR_DATA/res_producers-consumers.csv
-    cat $DIR_DATA/res_producers-consumers.csv
-fi
-
-# ---------------------------------------------
-# RUN TESTS PHILOSOPHERS
-# ---------------------------------------------
-if [ $EXPERIMENT -eq 3 ]; then
-    echo -e "\n\e[32m==========================\nPhilosophers tests\n==========================\e[0m"
-    bash $DIR_TESTS/perf_philosophers.sh $DIR_TARGET $DIR_DATA/res_philosophers.csv
-    cat $DIR_DATA/res_philosophers.csv
-fi
+cat $CSV
